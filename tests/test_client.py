7748bb765205e9b50e6e47b3c8e97556063cc70f
Jay Cary <jay@chloris.earth>
2024-01-31 14:59:12 -0500

    submit_site complete and unit tests passing

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,17 +1,19 @@
+import json
 import random
 import pytest
-from chloris_app_sdk.client import ChlorisAppClient
-from chloris_app_sdk.utils import is_token_expired
+
 import os
+
+# workaround to import from src directory
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))
+
+from chloris_app_sdk import ChlorisAppClient
 
 test_resources_path = os.path.join(os.path.dirname(__file__), "test_resources")
 
 TEST_ORGANIZATION_ID = 'dev-0183c887-4cb1-752d-8767-d328337a4312'
-TEST_API = 'https://api-dev.chloris.earth/api/'
-
-def test_is_token_expired():
-    test_token_expired = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE2OTA5MjE3NDN9.W8ELUqPcKB6bgIVT_5tMuTJ7mTFnDNFnBTzad4FCZek"
-    assert is_token_expired(test_token_expired) == True
+TEST_API = 'https://app-dev.chloris.earth/api/'
 
 
 # @pytest.mark.skip(reason="This is an integration-level test and makes real calls to AWS")
@@ -28,19 +30,40 @@ def test_client() -> None:
     test_file = f"protected/{creds['IdentityId']}/{random.randint(0, 1000000)}_test.txt"
     client._upload_file(key=test_file, body="test")
     client._get_object_metadata(key=test_file)
+    # test geojson boundary upload succeeds (local path)
+    boundary_path = client.upload_boundary_geojson(os.path.join(test_resources_path, "test_small_site.geojson"))
+    assert boundary_path is not None
+    # test geojson boundary upload succeeds (local file)
+    with open(os.path.join(test_resources_path, "test_small_site.geojson"), "r") as f:
+        boundary_path = client.upload_boundary_geojson(json.load(f))
+    assert boundary_path is not None
+    # test geojson boundary upload succeeds (remote url)
+    boundary_path = client.upload_boundary_geojson("https://raw.githubusercontent.com/chloris-geospatial/chloris-app-sdk/main/tests/test_resources/test_small_site.geojson")
+    assert boundary_path is not None
+
     # test boundary upload succeeds
-    with open(os.path.join(test_resources_path, "test_small_site.geojson"), "rt", encoding="utf-8") as f:
-        test_file = f.read()
-    boundary_path = client.upload_boundary_file(test_file)
+    boundary_path = client.upload_boundary_file(os.path.join(test_resources_path, "test_small_site.geojson"))
     assert boundary_path is not None
     # test boundary upload fails (too large)
-    with open(os.path.join(test_resources_path, "test_too_big_site.geojson"), "rt", encoding="utf-8") as f:
-        test_file = f.read()
     with pytest.raises(Exception):
-        client.upload_boundary_file(test_file)
+        client.upload_boundary_file(os.path.join(test_resources_path, "test_too_big_site.geojson"))
+
+    # test site submission succeeds
+    reporting_unit = client.submit_site(
+        label="site 1",
+        boundary_path=os.path.join(test_resources_path, "test_small_site.geojson"),
+        description="test description",
+        tags = ['test'],
+        dryrun=True,
+        notify = False,
+        period_change_start_year=2000,
+        period_change_end_year=2023
+    )
+    assert reporting_unit.get('reportingUnitId') is not None
+
+
 
 def test_list_active_sites():
-    from chloris_app_sdk import ChlorisAppClient
 
     client = ChlorisAppClient (TEST_ORGANIZATION_ID, api_endpoint=TEST_API)
 
@@ -49,7 +72,6 @@ def test_list_active_sites():
     assert len(reporting_units) > 0
 
 def test_get_reporting_unit():
-    from chloris_app_sdk import ChlorisAppClient
 
     client = ChlorisAppClient (TEST_ORGANIZATION_ID, api_endpoint=TEST_API)
 
@@ -60,6 +82,6 @@ def test_get_reporting_unit():
 
     assert reporting_unit_entry['reportingUnitId'] == reporting_unit_id
     assert reporting_unit_entry['layersConfig'] is not None
-    assert reporting_unit_entry['stats'] is not None
-    assert reporting_unit_entry['downloads'] is not None
+    assert reporting_unit_entry['annualYears'] is not None
+    # assert reporting_unit_entry['downloads'] is not None
 
